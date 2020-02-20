377e32f199bd85cd74bca77da501d1b90cdc112f
Andrew Kaurov <56529033+Kaurov2000@users.noreply.github.com>
2020-02-20 14:27:05 +0500

    Add files via upload

---
+++ rpms_procedures.sql
@@ -0,0 +1,63 @@
+DELIMITER //
+-- Проверка правильности ссылки на вторую сторону в договорах
+DROP TRIGGER IF EXISTS add_contract_dependencies_check //
+CREATE TRIGGER add_contract_dependencies_check BEFORE INSERT ON contracts
+FOR EACH ROW 
+BEGIN
+	IF NEW.contract_type_id = (SELECT id FROM contract_types ct WHERE contract_type = 'income')
+		AND 
+		NEW.counterpart_id NOT IN (SELECT id from clients)
+	THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 
+		'Insert cancelled: counterpart_id for income contracts must reference clients.id';
+	ELSEIF NEW.contract_type_id = (SELECT id FROM contract_types ct WHERE contract_type = 'expense')
+		AND 
+		NEW.counterpart_id NOT IN (SELECT id from contractor_service_type_links)
+	THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 
+		'Insert cancelled: counterpart_id for expense contracts must reference contractor_service_type_links.id';
+	 END IF;
+END//
+-- Проверка на наличие неоплаченных счетов. Функция возвращает колическтво
+-- просроченных счетов по объекту
+DROP FUNCTION IF EXISTS overdue_payments_count //
+CREATE FUNCTION overdue_payments_count(input_client_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE bills_count INT;
+	SELECT count(b.id) INTO bills_count 
+	FROM bills b INNER JOIN contracts c ON b.contract_id = c.id
+		INNER JOIN contract_types ct ON ct.id =c.contract_type_id 
+			AND ct.contract_type = 'expense'
+		INNER JOIN assets a ON a.id = c.asset_id
+			AND a.client_id = input_client_id
+	WHERE b.due_date < CURRENT_DATE() AND b.payment_date is NULL;
+	RETURN coalesce(bills_count, 0);
+END//
+
+-- Процедура собирает таблицу с фактами просрочки платежа клиентами 
+DROP PROCEDURE IF EXISTS update_overdue_payments_log//
+CREATE PROCEDURE update_overdue_payments_log()
+BEGIN
+	INSERT INTO client_overdue_payments_log
+		(log_entry_date, user_id, asset_id, bill_id, overdue_days, client_id, client_name)
+	SELECT 
+		CURRENT_DATE() as log_entry_date,
+		a.user_id,
+		c.asset_id,
+		b.id as bill_id,
+		DATEDIFF(CURRENT_DATE(), b.due_date) as overdue_days,
+		cli.id as client_id,
+		CONCAT(cli.last_name, ' ', cli.first_name) as client_name
+	FROM bills b INNER JOIN contracts c ON b.contract_id = c.id 
+		INNER JOIN contract_types ct ON ct.id =c.contract_type_id 
+			AND ct.contract_type = 'income'
+		INNER JOIN clients cli ON c.counterpart_id = cli.id
+		INNER JOIN assets a ON a.id = c.asset_id 
+	WHERE b.due_date < CURRENT_DATE() AND b.payment_date is NULL;
+END//
+
+DELIMITER ;
+
+-- CALL update_overdue_payments_log();
+	
+
+
---
+++ rpms_views.sql
@@ -0,0 +1,81 @@
+-- Представление с активами всех польователей
+DROP VIEW IF EXISTS user_assets;
+CREATE VIEW user_assets AS
+SELECT 
+	u.id as user_id,
+	a.id as asset_id,
+	a.asset_type_id,
+	ast.asset_type,
+	a.name,
+	a.country,
+	a.city,
+	a.address
+FROM users u INNER JOIN assets a ON u.id = a.user_id 
+	INNER JOIN asset_types ast ON a.asset_type_id = ast.id
+ORDER BY user_id;
+	
+-- Представление со всеми расчетами по активам
+DROP VIEW IF EXISTS asset_bills;
+CREATE VIEW asset_bills AS
+SELECT
+	a.user_id,
+	a.id as asset_id,
+	ct.contract_type ,
+	c.name as contract_name,
+	b.id as bill_id,
+	b.issue_date,
+	b.bill_amount,
+	b.due_date,
+	b.payment_date 
+FROM assets a LEFT JOIN contracts c ON a.id = c.asset_id
+	INNER JOIN contract_types ct ON c.contract_type_id = ct.id 
+	LEFT JOIN bills b ON b.contract_id = c.id
+ORDER BY user_id, asset_id, issue_date;
+
+
+-- Представление "Расчеты с арендаторами"
+DROP VIEW IF EXISTS client_bills;
+CREATE VIEW client_bills AS
+SELECT
+	a.id as asset_id,
+	cl.first_name,
+	cl.last_name,
+	b.id as bill_id,
+	b.issue_date,
+	b.bill_amount,
+	b.due_date,
+	b.payment_date 
+FROM assets a LEFT JOIN contracts c ON a.id = c.asset_id 
+	AND c.contract_type_id = 
+		(SELECT ct.id FROM contract_types ct WHERE ct.contract_type = 'income')
+ 	LEFT JOIN clients cl ON cl.id = c.counterpart_id 
+	LEFT JOIN bills b ON b.contract_id = c.id
+ORDER BY asset_id, last_name, issue_date;
+
+-- Представление "Расчеты с поставщиками услуг"
+DROP VIEW IF EXISTS contractor_bills;
+CREATE VIEW contractor_bills AS
+SELECT 
+	a.id as asset_id,
+	b.id as bill_id,
+	cn.name as contractor,
+	st.service_type,
+	b.issue_date,
+	b.bill_amount,
+	b.due_date,
+	b.payment_date 
+FROM assets a LEFT JOIN contracts c ON a.id = c.asset_id 
+	AND c.contract_type_id = 
+		(SELECT ct.id FROM contract_types ct WHERE ct.contract_type = 'expense')
+ 	LEFT JOIN (contractor_service_type_links cs
+ 			INNER JOIN service_types st ON st.id = cs.service_type_id) 
+ 		ON cs.id = c.counterpart_id 
+	LEFT JOIN contractors cn ON cn.id = cs.contractor_id 
+ 	LEFT JOIN bills b ON b.contract_id = c.id
+ORDER BY asset_id, contractor, issue_date;
+
+
+
+
+
+
